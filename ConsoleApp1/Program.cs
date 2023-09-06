0790aaf6c8e64b5113570caf35877e0725c86f8a
Sergio <buchanansergio@gmail.com>
2023-09-06 13:50:54 -0500

    Updates to Extend Exercise without coding for removal of all string instances of search term

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* See https://aka.ms/new-console-template for more information
 Console.WriteLine("INPUT/OUTPUT");
 Console.WriteLine("What is your name?");
 string myName = Console.ReadLine();
@@ -25,22 +25,33 @@ Console.WriteLine("\n");
 int milesPerGallon = milesDriven / gasConsumed;
 Console.WriteLine("\n");
 Console.WriteLine("Your miles-per-gallon: " + milesPerGallon + "mpg");
-Console.WriteLine("\n");
+Console.WriteLine("\n");*/
 Console.WriteLine("STRINGS");
 string opening = @"Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
 Console.WriteLine("What term would you like to search for?");
 string searchTerm = Console.ReadLine();
 string compSearchTerm = searchTerm.ToLower();
 string compOpening = opening.ToLower();
+string tripOpening = compOpening.Remove(opening.IndexOf(searchTerm), searchTerm.Length);
 
 if (compOpening.IndexOf(compSearchTerm, 0) != -1)
 {
-    Console.Write("true");
+    Console.WriteLine("true"); 
+    Console.WriteLine(compOpening.IndexOf(compSearchTerm));
+    Console.WriteLine(compSearchTerm.Length);
+    Console.WriteLine(compOpening.Remove(opening.IndexOf(searchTerm), searchTerm.Length));
+
 }
 else
 {
     Console.WriteLine("false");
 };
+
+
+//Console.WriteLine(compOpening.Remove(opening.IndexOf(searchTerm), searchTerm.Length));//
+//Console.WriteLine(compOpening.Remove(opening.IndexOf(searchTerm), opening.IndgexOf(searchTerm))); 
+
+
 
 
 
